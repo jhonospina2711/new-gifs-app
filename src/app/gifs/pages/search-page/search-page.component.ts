36dd5c4cf8a1db28566d8d5c4a2142f6493b90da
Jhon Ospina <jhonospina2711@gmail.com>
2025-04-25 00:02:08 -0500

    fix: add gifs pages

---
+++ src/app/gifs/pages/search-page/search-page.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-page.component.html',
+})
+export default class SearchPageComponent {}
